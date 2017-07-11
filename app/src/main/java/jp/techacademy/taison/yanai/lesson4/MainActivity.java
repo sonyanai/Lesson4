96837f22ea12398fe575bc4cf5ea54e6c9c8f4dc
sonyanai <taison0930@outlook.jp>
2017-07-11 23:51:56 +0900

    excepterro

--- app/src/main/java/jp/techacademy/taison/yanai/lesson4/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/lesson4/MainActivity.java
@@ -2,6 +2,7 @@ package jp.techacademy.taison.yanai.lesson4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setText("ボタン");
     }
 }
