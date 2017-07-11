ba4d1a9fe785097ff07efb1a1cffbfb12b28d242
sonyanai <taison0930@outlook.jp>
2017-07-12 00:05:31 +0900

    excepterro

--- app/src/main/java/jp/techacademy/taison/yanai/lesson4/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/lesson4/MainActivity.java
@@ -2,9 +2,11 @@ package jp.techacademy.taison.yanai.lesson4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button1 = (Button) findViewById(R.id.button1);
-        button1.setText("ボタン");
+        button1.setOnClickListener(this);
+        //setOnClickListenerメソッドの引数にはonClickメソッドを実装したクリックリスナークラスを渡す必要があります。
+        //今回はMainActivityクラス自身(this)を設定しonClickメソッドを実装しています。
+    }
+    @Override
+    public void onClick(View v) {
+        Log.d("UI_PARTS","ボタンをタップしました");
     }
 }
