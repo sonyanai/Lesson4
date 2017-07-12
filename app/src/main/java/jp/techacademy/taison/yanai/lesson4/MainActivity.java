b2291b31e1bd036551efc4d57239a96896cd8d55
sonyanai <taison0930@outlook.jp>
2017-07-12 10:21:34 +0900

    ui

--- app/src/main/java/jp/techacademy/taison/yanai/lesson4/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/lesson4/MainActivity.java
@@ -1,11 +1,14 @@
 package jp.techacademy.taison.yanai.lesson4;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,12 +23,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
 
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
         mTextView = (TextView) findViewById(R.id.textView);
         mEditText = (EditText) findViewById(R.id.editText);
     }
     @Override
-    public void onClick(View v){
-        mTextView.setText(mEditText.getText().toString());
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            mTextView.setText(mEditText.getText().toString());
+        } else if (v.getId() == R.id.button2) {
+            showAlertDialog();
+        }
     }
 
+    private void showAlertDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("タイトル");
+        alertDialogBuilder.setMessage("メッセージ");
+
+        alertDialogBuilder.setPositiveButton("肯定",
+                new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog,int which){
+                        Log.d("UI_PARTS","肯定ボタン");
+                    }
+                });
+
+        alertDialogBuilder.setPositiveButton("中立",
+                new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog,int which){
+                        Log.d("UI_PARTS","中立ボタン");
+                    }
+                });
+
+        alertDialogBuilder.setPositiveButton("否定",
+                new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog,int which){
+                        Log.d("UI_PARTS","否定ボタン");
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
 }
